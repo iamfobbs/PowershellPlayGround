2abb32a55568e960b9887e049eeb9aaa162025f7
Dennis Ofoborh <dofoborh@hotmail.co.uk>
2016-04-13 23:10:29 +0100

    ps for get disk info

---
+++ Fundamentals/Get-DiskInfo.ps1
@@ -0,0 +1,11 @@
+﻿param(
+    [string]$ComputerName= 'localhost',
+    [int]$DriveType =3
+
+)
+
+Get-WmiObject -Class win32_logicaldisk -Filter "DriveType=DriveType" -ComputerName $ComputerName | 
+    Select-Object -property @{n='Drive';e={$_.DeviceID}}, 
+                            @{n='ComputerName';e={$_.__SERVER}},
+                            @{n='FreeSpace(GB)';e={$_.FreeSpace / 1GB -as [int]}},
+                            @{n='Size';e={$_.Size / 1GB -as [int]}}
